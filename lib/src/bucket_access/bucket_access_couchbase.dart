5095dd367012b5cb54cb137509f4d8e4415fb6c7
Stéphane Seguin <stephseguin93@gmail.com>
2014-12-08 22:31:21 +0100

    Close all connexions event if an error occured

--- lib/src/bucket_access/bucket_access_couchbase.dart
+++ lib/src/bucket_access/bucket_access_couchbase.dart
@@ -4,110 +4,79 @@ class BucketAccessCouchbase extends ABucketAccess {
   
   BucketAccessCouchbase(String bucket) : super(bucket);
 
-  
-  
   Future set(DBObject object) {
-    if (object.key == null || object.key == "")
+    if (object.key == null || object.key == "") {
       throw "FileTeCouch cannot write a value with an empty key";
-    
-    return CouchbaseCluster.connect(bucket)
-      .then( (CouchClient client) {
-        return client.set(object.key, UTF8.encode(JSON.encode(object.value)), exptime: object.ttl, cas: object.revision)
-            .then( (val) {
-              client.close();
-              
-              if (val == false)
+    }
+    return CouchbaseCluster.connect(bucket).then((CouchClient client) {
+      return client.set(object.key, UTF8.encode(JSON.encode(object.value)), exptime: object.ttl, cas: object.revision)
+          .then((val) {
+            if (val == false) {
                 throw "FileTeCouch cannot write the object \"" + object.key + "\" in db";
-              
-              return object;
-            });
-      });
+            }
+            return object;
+          })
+          .whenComplete(() => client.close());
+    });
   }
   
   Future get(String key) {
-    return CouchbaseCluster.connect(bucket)
-      .then( (CouchClient client) {
-        return client.gets(key)
-            .then( (GetResult val) {
-              client.close();
-              return val;
-            });
+    return CouchbaseCluster.connect(bucket).then((CouchClient client) {
+      return client.gets(key)
+          .then((val) => val)
+          .whenComplete(() => client.close());
       })
-      
       .then(DBObject.entryToDBObject);
   }
   
   Future getAll(List<String> keys) {
-    return CouchbaseCluster.connect(bucket)
-      .then( (CouchClient client) {
-        return client.getsAll(keys).toList()
-            .then( (List<GetResult> val) {
-              client.close();
-              return val;
-            });
+    return CouchbaseCluster.connect(bucket).then((CouchClient client) {
+      return client.getsAll(keys).toList()
+        .then((val) => val)
+        .whenComplete(() => client.close());
     })
-    
     .then(DBObject.entriesToDBObject);
   }
   
   Future delete(String key) {
-    return CouchbaseCluster.connect(bucket)
-      .then( (CouchClient client) {
-        return client.delete(key)
-            .then( (val) {
-              client.close();
-              return val;
-            });
-      });
+    return CouchbaseCluster.connect(bucket).then((CouchClient client) {
+      return client.delete(key)
+          .then((val) => val)
+          .whenComplete(() => client.close());
+    });
   }  
-  
-  
-  
-  
 
   Future increment(DBObject obj) {
-    return CouchbaseCluster.connect(bucket)
-          .then( (CouchClient client) {
-            return client.increment(obj.key, obj.value)
-                .then( (val) {
-                  client.close();
-                  return val;
-                });
-          });
+    return CouchbaseCluster.connect(bucket).then((CouchClient client) {
+      return client.increment(obj.key, obj.value)
+        .then((val) => val)
+        .whenComplete(() => client.close());
+    });
   }
+
   Future decrement(DBObject obj) {
-    return CouchbaseCluster.connect(bucket)
-              .then( (CouchClient client) {
-                return client.decrement(obj.key, obj.value)
-                    .then( (val) {
-                      client.close();
-                      return val;
-                    });
-              });
+    return CouchbaseCluster.connect(bucket).then((CouchClient client) {
+      return client.decrement(obj.key, obj.value)
+        .then((val) => val)
+        .whenComplete(() => client.close());
+    });
   }
-  
-  
-  
   
   /**
    * Send a query to Couchbase Server for custom map/reduce algorythms
    */
   Future getView(String designDocumentName, String viewName, DBQuery query) {
-    return CouchbaseCluster.connect(bucket)
-                  .then( (CouchClient client) {
-                    return client.getView(designDocumentName, viewName)
-                        .then( (View view) {
-                          if (view == null)
-                            throw "Unknown view";
-                          Query query = new Query();
-                          return client.query(view, query)
-                              .then( (results) {
-                                client.close();
-                                return (ViewObject.entriesToViewObject(view.bucketName, results.rows));
-                              });
-                        });
-                  });
+    return CouchbaseCluster.connect(bucket).then((CouchClient client) {
+      return client.getView(designDocumentName, viewName).then((View view) {
+        if (view == null) {
+          throw "Unknown view";
+        }
+        Query query = new Query();
+        return client.query(view, query)
+          .then((results) => (ViewObject.entriesToViewObject(view.bucketName, results.rows)))
+          .whenComplete(() => client.close());
+        });
+    });
   }
-  
   
 }
--- lib/src/bucket_access/bucket_access_memcached.dart
+++ lib/src/bucket_access/bucket_access_memcached.dart
@@ -6,82 +6,61 @@ class BucketAccessMemcached extends ABucketAccess {
 
   
   Future set(DBObject object) {
-    if (object.key == null || object.key == "")
+    if (object.key == null || object.key == "") {
           throw "FileTeCouch cannot write a value with an empty key";
-        
-        return CouchbaseCluster.connect(bucket)
-          .then( (MemcachedClient client) {
-            return client.set(object.key, UTF8.encode(JSON.encode(object.value)), exptime: object.ttl, cas: object.revision)
-                .then( (val) {
-                  client.close();
-                  
-                  if (val == false)
-                    throw "FileTeCouch cannot write the object \"" + object.key + "\" in db";
-                  
-                  return object;
-                });
-          });
+    }
+    return CouchbaseCluster.connect(bucket).then( (MemcachedClient client) {
+      return client.set(object.key, UTF8.encode(JSON.encode(object.value)), exptime: object.ttl, cas: object.revision)
+        .then((val) {
+          if (val == false) {
+            throw "FileTeCouch cannot write the object \"" + object.key + "\" in db";
+          }
+          return object;
+        })
+        .whenComplete(() => client.close());
+    });
   }
   
   Future get(String key) {
-    return CouchbaseCluster.connect(bucket)
-          .then( (MemcachedClient client) {
-            return client.gets(key)
-                .then( (GetResult val) {
-                  client.close();
-                  return val;
-                });
-          })
-          
-          .then(DBObject.entryToDBObject);
+    return CouchbaseCluster.connect(bucket).then((MemcachedClient client) {
+      return client.gets(key)
+        .then((GetResult val) => val)
+        .whenComplete(() => client.close());
+      })
+      .then(DBObject.entryToDBObject);
   }
   
   Future getAll(List<String> keys) {
-    return CouchbaseCluster.connect(bucket)
-      .then( (MemcachedClient client) {
-        return client.getsAll(keys).toList()
-            .then( (List<GetResult> val) {
-              client.close();
-              return val;
-            });
+    return CouchbaseCluster.connect(bucket).then((MemcachedClient client) {
+      return client.getsAll(keys).toList()
+        .then((val) => val)
+        .whenComplete(() => client.close());
       })
-      
       .then(DBObject.entriesToDBObject);
   }
   
   Future delete(String key) {
-    return CouchbaseCluster.connect(bucket)
-      .then( (MemcachedClient client) {
-        return client.delete(key)
-            .then( (val) {
-              client.close();
-              return val;
-            });
-      });
+    return CouchbaseCluster.connect(bucket).then((MemcachedClient client) {
+      return client.delete(key)
+          .then((val) => val)
+          .whenComplete(() => client.close());
+    });
   }
-  
-  
 
   Future increment(DBObject obj) {
-    return CouchbaseCluster.connect(bucket)
-          .then( (MemcachedClient client) {
-            return client.increment(obj.key, obj.value)
-                .then( (val) {
-                  client.close();
-                  return val;
-                });
-          });
+    return CouchbaseCluster.connect(bucket).then((MemcachedClient client) {
+      return client.increment(obj.key, obj.value)
+        .then((val) => val)
+        .whenComplete(() => client.close());
+    });
   }
   
   Future decrement(DBObject obj) {
-    return CouchbaseCluster.connect(bucket)
-              .then( (MemcachedClient client) {
-                return client.decrement(obj.key, obj.value)
-                    .then( (val) {
-                      client.close();
-                      return val;
-                    });
-              });
+    return CouchbaseCluster.connect(bucket).then((MemcachedClient client) {
+      return client.decrement(obj.key, obj.value)
+        .then((val) => val)
+        .whenComplete(() => client.close());
+    });
   }
   
   Future getView(String designDocumentName, String viewName, DBQuery query) {
